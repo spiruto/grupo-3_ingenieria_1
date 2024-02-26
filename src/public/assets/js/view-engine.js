07ecb7c33a736ec5c18a98ded2cdcaa39ccf6026
danielquesada-orangeloops <daniel.quesada@3shape.com>
2024-02-25 21:23:02 -0600

    fixed view engine

---
+++ src/public/assets/js/view-engine.js
@@ -0,0 +1,10 @@
+
+export const getComponent = async (componentName) => {
+    const fileRoute = `/components/${componentName}.html`;
+    const file = await (await fetch(fileRoute)).text();
+    if (file) {
+        console.log(file)
+        return file;
+    }
+    return null;
+};

c97a8983c281c65b938bbc53630cec82f1653258
danielquesada-orangeloops <daniel.quesada@3shape.com>
2024-03-06 13:34:31 -0600

    Update main.js

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -8,10 +8,10 @@ async function renderLayout() {
     const bodyEl = document.getElementById("root");
     const components = {};
     components["nav"] = await getComponent("nav");
-    components["footer"] = await getComponent("footer");
     components["leftNavBar"] = await getComponent("leftNavBar");
     components["rightNavBar"] = await getComponent("rightNavBar");
     components["content"] = await getComponent("content");
+    components["footer"] = await getComponent("footer");
     let layout = "";
     Object.values(components).forEach(component => {
         layout += component;

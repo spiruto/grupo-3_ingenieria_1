6561a3130296bc3beb26b06b69687772c88eaf00
danielquesada-orangeloops <daniel.quesada@3shape.com>
2024-03-06 13:46:54 -0600

    Update view-engine.js

--- src/public/assets/js/view-engine.js
+++ src/public/assets/js/view-engine.js
@@ -1,6 +1,6 @@
 
 export const getComponent = async (componentName) => {
-    const fileRoute = `/components/${componentName}.html`;
+    const fileRoute = `./components/${componentName}.html`;
     const file = await (await fetch(fileRoute)).text();
     if (file) {
         console.log(file)

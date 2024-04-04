7e0ecb83d38774ae6366ab45a882caa244dab802
Fabian Rovira <fabrovira@hotmail.com>
2024-04-03 18:11:48 -0600

    progreso 5

---
+++ src/public/assets/js/views/perfilVendedor.js
@@ -0,0 +1,17 @@
+import { getComponent } from "../view-engine.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderLayout();
+});
+
+async function renderLayout() {
+    const bodyEl = document.getElementById("root");
+    const components = {};
+    components["nav"] = await getComponent("nav");
+    components["footer"] = await getComponent("footer");
+    let layout = "";
+    Object.values(components).forEach(component => {
+        layout += component;
+    });
+    bodyEl.innerHTML = `${components.nav} ${bodyEl.innerHTML} ${components.footer}`;
+}

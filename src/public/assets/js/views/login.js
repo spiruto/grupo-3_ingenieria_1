0269f3192b3d35878746f3b16fb93d50dc42f56b
kryps22 <marc9622@gmail.com>
2024-04-05 16:00:37 -0600

    final dash

--- src/public/assets/js/views/login.js
+++ src/public/assets/js/views/login.js
@@ -1,7 +1,25 @@
-import { getComponent } from "../view-engine.js"
+import { getComponent } from "../view-engine.js";
+import validations from "../../validations.js";
+import validateForm from "../formValidator.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-    renderLayout();
+const validationRules = {
+    email: [
+        { 
+            validate: value => value.trim().length > 0, 
+            message: "Email is required" 
+        }
+    ],
+    password: [
+        { 
+            validate: value => value.trim().length > 0, 
+            message: "Password is required" 
+        }
+    ]
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await renderLayout();
+    validateForm("formRegistroVendedor", validationRules);
 });
 
 async function renderLayout() {
@@ -9,9 +27,7 @@ async function renderLayout() {
     const components = {};
     components["nav"] = await getComponent("nav");
     components["footer"] = await getComponent("footer");
-    let layout = "";
-    Object.values(components).forEach(component => {
-        layout += component;
-    });
-   bodyEl.innerHTML = `${components.nav} ${bodyEl.innerHTML} ${components.footer}`;
+    
+    let layout = Object.values(components).join(""); // Join components into a single string
+    bodyEl.innerHTML = layout + bodyEl.innerHTML; // Add components to the beginning of bodyEl's content
 }
